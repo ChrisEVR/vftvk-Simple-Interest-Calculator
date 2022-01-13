373b74296fb1c6ff0d4d75c527a6c540c1255650
Christian Eduardo Vargas <christianvargasraudales@gmail.com>
2022-01-13 01:21:57 +0000

    Final Project 2

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function compute()
         alert("Enter a positive number");
     }else{
         p = document.getElementById("principal").value;
-        document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+        document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
     }
 }
 
