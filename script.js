3855790a3469d96929489b5288bb2cf96a954bfb
Christian Eduardo Vargas <christianvargasraudales@gmail.com>
2022-01-13 01:17:54 +0000

    Final Project

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+
+//Function to compute
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = principal * years * rate / 100;
+    let amount = principal * interest;
+    let year = new Date().getFullYear() + parseInt(years);
+
+    if(principal <= 0){
+        alert("Enter a positive number");
+    }else{
+        p = document.getElementById("principal").value;
+        document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    }
 }
+
+//Function to compute rate
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML=rateval;
+}
+
+
         
